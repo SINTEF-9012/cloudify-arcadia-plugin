cf4148303e1b4763a28d1dc5b1b4f6e44a53e3b2
vassik <anatoly.vasilevskiy@sintef.no>
2017-06-22 15:26:59 +0200

    some comments

--- plugin/api/rest_api_client.py
+++ plugin/api/rest_api_client.py
@@ -2,6 +2,10 @@
 
 class ARCADIACompResponse(object):
 
+	def __init__(self, cepcid=None, ecepcid=None):
+		self._cepcid = cepcid
+		self._ecepid = ecepcid
+
 	@property
 	def cepcid(self):
 		return self._cepcid
@@ -18,11 +22,17 @@ class ARCADIARestAPIClient(object):
 
 	def get_component_info(self, cnid):
 		#make a call here
-		response = ARCADIACompResponse()
+		#if request_fals:
+		#	return {'rc' : 1, 'message' : reason}
+
+		response = ARCADIACompResponse(cepcid='cepcid', ecepcid='ecepcid')
 		return {'rc' : 0, 'message' : 'SUCCESS', 'response' : response}
 
 
 	def register_service_graph(self, service_tree):
 		#make a call here
 		#service_tree.print_element will print xml, or any other format which is set by ARCADIAClientFacade
+		#if request_fals:
+		#	return {'rc' : 1, 'message' : reason}
+		
 		return {'rc' : 0, 'message' : 'SUCCESS'}
