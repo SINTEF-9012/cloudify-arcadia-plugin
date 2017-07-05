1430a6c903ea8d2060f1517548856944921dbf98
Franck Chauvel <franck.chauvel@gmail.com>
2017-07-05 17:32:04 +0200

    Minimalist integration of the register client/API

--- plugin/api/rest_api_client.py
+++ plugin/api/rest_api_client.py
@@ -1,5 +1,15 @@
 from plugin.api.responses import ARCADIACompResponse
 
+from requests import post, codes
+
+
+HOST_NAME = "34.250.221.100"
+PORT = 80
+
+URL = "http://{host}:{port}".format(
+        host=HOST_NAME,
+        port=PORT)
+
 
 class ARCADIARestAPIClient(object):
 
@@ -19,5 +29,11 @@ class ARCADIARestAPIClient(object):
 		#service_tree.print_element will print xml, or any other format which is set by ARCADIAClientFacade
 		#if request_fals:
 		#	return {'rc' : 1, 'message' : reason}
-
+		payload = service_tree.print_element
+		response = post(URL + "/register",
+                     headers={"content-type": "application/xml"},
+                     data=payload)
+                if response.status_code != codes.ok:
+                        message = "Unable to post service graph at {url}".format(url=URL)
+                        raise RuntimeError(message)
 		return {'rc' : 0, 'message' : 'SUCCESS'}
