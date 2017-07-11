253343560d9f008c013037b0501d415e4a715de2
vassik <anatoly.vasilevskiy@sintef.no>
2017-07-11 14:56:53 +0200

    rest client is complete now

--- plugin/api/rest_api_client.py
+++ plugin/api/rest_api_client.py
@@ -19,31 +19,31 @@ class ARCADIARestAPIClient(object):
 		pass
 
 	def get_component_info(self, cnid):
-                response = get(URL + "/components/" + cnid,
-                               headers = {"Accept": "application/xml"})
-                if response.status_code not in [200, 204]:
-                        message = ERROR_CANNOT_FETCH_CNID.format(
-                                cnid=cnid,
-                                response=response.text)
-                        return { "rc": 1, "message": message}
-                node = etree.fromstring(response.text)
-                nid = node.find("NID").text
-                cnid = node.find("CNID").text
-		response = ARCADIACompResponse(cepcid=nid, ecepcid=cnid)
+		response = get(URL + "/components/" + cnid,
+						headers = {"Accept": "application/xml"})
+
+		if response.status_code not in [200, 204]:
+			message = ERROR_CANNOT_FETCH_CNID.format(
+							cnid=cnid,
+							response=response.text)
+			return { "rc": 1, "message": message}
+
+		node = etree.fromstring(response.text)
+		_cepcid = node.find("CEPCID").text if node.find("CEPCID") is not None else None
+		_ecepid = node.find("ECEPID").text if node.find("ECEPID") is not None else None
+		response = ARCADIACompResponse(cepcid=_cepcid, ecepcid=_ecepid)
 		return {'rc' : 0, 'message' : 'SUCCESS', 'response' : response}
 
 	def register_service_graph(self, service_tree):
-		#make a call here
-		#service_tree.print_element() will print xml, or any other format which is set by ARCADIAClientFacade
-		#if request_fals:
-		#	return {'rc' : 1, 'message' : reason}
 		payload = service_tree.print_element()
 		response = post(URL + "/register",
 				headers={"content-type": "application/xml"},
 				data=payload)
+
 		if response.status_code not in [200, 204]:
-                        message = ERROR_CANNOT_POST.format(
-                                url=URL,
-                                response=response.text)
-		        return {'rc' : 1, 'message' : message}
+			message = ERROR_CANNOT_POST.format(
+								url=URL,
+								response=response.text)
+			return {'rc' : 1, 'message' : message}
+
 		return {'rc' : 0, 'message' : 'SUCCESS'}
