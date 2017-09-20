974101349e9c2f9bf02e5e80bc5323760f8cb230
vassik <anatoly.vasilevskiy@sintef.no>
2017-09-20 16:57:56 +0200

    new e2e test case we want to pass with new components

---
+++ plugin/tests/test_plugin_e2e_new_comp.py
@@ -0,0 +1,48 @@
+import unittest
+from os import path
+from cloudify.test_utils import workflow_test
+from plugin.context import actx
+from plugin.srv_graph.graph_element import ServiceGraphElement
+
+
+class TestPlugin(unittest.TestCase):
+
+	def setUp(self):
+		actx._components = dict()
+		actx._relationships = dict()
+		actx._service_graph_instance = None
+		actx._client = None
+		actx._service_graph_tree = None
+
+	@workflow_test(path.join('blueprint', 'blueprint_e2e_nem_comp.yaml'),
+			resources_to_copy=[path.join('blueprint', 'test_plugin.yaml')])
+	def test_install_arcadia_workflow_e2e_new_comp(self, cfy_local):
+		cfy_local.execute('install_arcadia', task_retries=0)
+
+		service_graph = actx.client._service_graph_tree
+		self.assertTrue(isinstance(service_graph, ServiceGraphElement))
+		self.assertTrue(len(service_graph.components) == 2)
+		self.assertTrue(len(service_graph.policies) == 1)
+
+		def find_component(array, component):
+			for comp in array:
+				if comp._instance._node_instance['name'] == component:
+					return comp
+			return
+
+		mysql_comp = find_component(service_graph.components, 'mysql')
+		wp_comp = find_component(service_graph.components, 'wordpress')
+		self.assertTrue(mysql_comp)
+		self.assertTrue(wp_comp)
+		self.assertTrue(len(wp_comp.dependencies) == 1)
+		self.assertTrue(wp_comp.dependencies[0].target._instance == mysql_comp._instance)
+
+		cepcid = mysql_comp._instance._node_instance.runtime_properties.get('cepcid')
+		ecepid = mysql_comp._instance._node_instance.runtime_properties.get('ecepid')
+		self.assertEquals(cepcid, 'mysqltcp_cepcid_new_comp')
+		self.assertEquals(ecepid, 'mysqltcp_new_comp')
+
+		cepcid = wp_comp._instance._node_instance.runtime_properties.get('cepcid')
+		ecepid = wp_comp._instance._node_instance.runtime_properties.get('ecepid')
+		self.assertIsNone(cepcid)
+		self.assertIsNone(ecepid)
